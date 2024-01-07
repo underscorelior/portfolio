2ad30d50d21c346e0d1ad9c09c2628037c60c3cf
underscorelior <newlioryt@gmail.com>
2024-01-07 03:03:04 -0800

    Improve styling

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,7 @@ import { SiDiscord, SiGithub, SiGmail } from 'react-icons/si';
 class Header extends Component {
 	render() {
 		return (
-			<header className="relative mx-auto flex w-full justify-center pt-12 xl:pt-28 2xl:pb-[8.5%] 2xl:pt-[13.5rem] 3xl:pb-[10rem] 3xl:pt-[18rem] 4xl:pb-[20rem] 4xl:pt-[25rem]">
+			<header className="relative mx-auto flex w-full justify-center pt-12 xl:pt-28 2xl:pb-[8.5%] 2xl:pt-[13.5rem] 3xl:pb-[10rem] 3xl:pt-[18rem] 4xl:pb-[15rem] ">
 				<div className="relative max-w-lg blur-xl lg:blur-3xl">
 					<div className="absolute -left-10 top-10 h-40 w-40 animate-blob rounded-full bg-fuchsia-400 mix-blend-multiply blur-xl filter sm:h-60 sm:w-60 lg:-left-20 lg:h-80 lg:w-80 lg:blur-3xl"></div>
 					<div className="animation-delay-2000 absolute left-32 top-0 h-40 w-40 animate-blob rounded-full bg-teal-300 mix-blend-multiply blur-xl filter sm:h-60 sm:w-60 lg:left-64 lg:h-80 lg:w-80 lg:blur-3xl"></div>
